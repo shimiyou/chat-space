6719fee5d8224d044b4b6325417080bbfc7adf54
shimiyou <0ri6ku1jo9apple@gmail.com>
2019-10-24 21:42:00 +0900

    Create group create function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "messages#index"
-
-  resources :messages, only: :index
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
